3f65a27432ae07fa239a8cffd64624162359e38c
Matt Campbell <matt@soupmatt.com>
2012-02-01 13:13:02 -0600

    Update rake and remove unneeded require from spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,16 +1,11 @@
 lib_path = File.expand_path(File.dirname(__FILE__) + "/../lib")
 $LOAD_PATH.unshift lib_path unless $LOAD_PATH.include?(lib_path)
 
-require 'rubygems'
-require 'bundler/setup'
-
 require 'action_controller'
 
 require 'rspec/core'
 require 'rspec/core/formatters/base_text_formatter'
 require 'rspec-rails23'
-
-#gem "mocha"
 
 module Rspec
   module Core
