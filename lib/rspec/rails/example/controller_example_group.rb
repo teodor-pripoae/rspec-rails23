b0e7bdf0af6ce0bd76d115099e3bb88b33d7224c
Matt Campbell <matt@soupmatt.com>
2012-06-06 16:28:00 -0500

    Refactor the way controller example groups is handled

--- lib/rspec/rails/example/controller_example_group.rb
+++ lib/rspec/rails/example/controller_example_group.rb
@@ -1,6 +1,52 @@
+require 'action_controller'
+require 'action_controller/test_case'
+
 module RSpec
   module Rails
     module ControllerExampleGroup
+      extend ActiveSupport::Concern
+      include RailsExampleGroup
+      include ActionController::TestProcess
+      include ActionController::TestCase::Assertions
+      include ActionController::TestCase::RaiseActionExceptions
+
+      attr_reader :request, :response, :controller
+
+      def route_for(options)
+        ActionController::Routing::Routes.reload if ActionController::Routing::Routes.empty?
+        ActionController::Routing::Routes.generate(options)
+      end
+
+      def params_from(method, path)
+        ActionController::Routing::Routes.reload if ActionController::Routing::Routes.empty?
+        ActionController::Routing::Routes.recognize_path(path, :method => method)
+      end
+
+      # @private
+      def controller_class
+        described_class
+      end
+
+      included do
+        subject { @controller }
+
+        metadata[:type] = :controller
+
+        before do
+          @request = ActionController::TestRequest.new
+          @response = ActionController::TestResponse.new
+
+          if klass = controller_class
+            @controller ||= klass.new rescue nil
+          end
+
+          if @controller
+            @controller.request = @request
+            @controller.params = {}
+            @controller.send(:initialize_current_url)
+          end
+        end
+      end
     end
   end
 end
---
+++ lib/rspec/rails/example/rails_example_group.rb
@@ -0,0 +1,7 @@
+module RSpec
+  module Rails
+    module RailsExampleGroup
+      extend ActiveSupport::Concern
+    end
+  end
+end
