ff89280f4c7b1e402db74d56a92d87d9e9de2b0c
Matt Campbell <matt@soupmatt.com>
2012-12-18 13:41:02 -0600

    Add anonymous controller and custom route support to controller examples

--- lib/rspec/rails/example/controller_example_group.rb
+++ lib/rspec/rails/example/controller_example_group.rb
@@ -1,6 +1,10 @@
 require 'action_controller'
 require 'action_controller/test_case'
 
+RSpec.configure do |config|
+  config.add_setting :infer_base_class_for_anonymous_controllers, :default => false
+end
+
 module RSpec
   module Rails
     module ControllerExampleGroup
@@ -12,6 +16,116 @@ module RSpec
       include RSpec::Rails::ViewRendering
       include RSpec::Rails::Matchers::RenderTemplate
       include RSpec::Rails::Matchers::RedirectTo
+
+      module ClassMethods
+        # @private
+        def controller_class
+          described_class
+        end
+
+        # Supports a simple DSL for specifying behavior of ApplicationController.
+        # Creates an anonymous subclass of ApplicationController and evals the
+        # `body` in that context. Also sets up implicit routes for this
+        # controller, that are separate from those defined in "config/routes.rb".
+        #
+        # @note Due to Ruby 1.8 scoping rules in anoymous subclasses, constants
+        #   defined in `ApplicationController` must be fully qualified (e.g.
+        #   `ApplicationController::AccessDenied`) in the block passed to the
+        #   `controller` method. Any instance methods, filters, etc, that are
+        #   defined in `ApplicationController`, however, are accessible from
+        #   within the block.
+        #
+        # @example
+        #
+        #     describe ApplicationController do
+        #       controller do
+        #         def index
+        #           raise ApplicationController::AccessDenied
+        #         end
+        #       end
+        #
+        #       describe "handling AccessDenied exceptions" do
+        #         it "redirects to the /401.html page" do
+        #           get :index
+        #           response.should redirect_to("/401.html")
+        #         end
+        #       end
+        #     end
+        #
+        # If you would like to spec a subclass of ApplicationController, call
+        # controller like so:
+        #
+        #     controller(ApplicationControllerSubclass) do
+        #       # ....
+        #     end
+        def controller(base_class = nil, &body)
+          base_class ||= RSpec.configuration.infer_base_class_for_anonymous_controllers? ?
+            controller_class :
+            ApplicationController
+
+          metadata[:example_group][:described_class] = Class.new(base_class) do
+            def self.name; "AnonymousController"; end
+          end
+          metadata[:example_group][:described_class].class_eval(&body)
+
+          with_routing do |set|
+            set.draw do |map|
+              map.resources :anonymous
+              map.connect 'anonymous/:action', :controller => :anonymous
+              map.connect 'anonymous/:action/:id', :controller => :anonymous
+            end
+          end
+        end
+
+        def with_routing(&route_set)
+          around(:each) do |example|
+            begin
+              real_routes = ActionController::Routing::Routes
+              ActionController::Routing.module_eval { remove_const :Routes }
+
+              temporary_routes = ActionController::Routing::RouteSet.new
+              ActionController::Routing.module_eval { const_set :Routes, temporary_routes }
+
+              route_set.call temporary_routes
+              example.run
+            ensure
+              if ActionController::Routing.const_defined? :Routes
+                ActionController::Routing.module_eval { remove_const :Routes }
+              end
+              ActionController::Routing.const_set(:Routes, real_routes) if real_routes
+            end
+          end
+        end
+
+      end
+
+      module BypassRescue
+        def rescue_with_handler(exception)
+          raise exception
+        end
+      end
+
+      # Extends the controller with a module that overrides
+      # `rescue_with_handler` to raise the exception passed to it.  Use this to
+      # specify that an action _should_ raise an exception given appropriate
+      # conditions.
+      #
+      # @example
+      #
+      #     describe ProfilesController do
+      #       it "raises a 403 when a non-admin user tries to view another user's profile" do
+      #         profile = create_profile
+      #         login_as profile.user
+      #
+      #         expect do
+      #           bypass_rescue
+      #           get :show, :id => profile.id + 1
+      #         end.to raise_error(/403 Forbidden/)
+      #       end
+      #     end
+      def bypass_rescue
+        controller.extend(BypassRescue)
+      end
 
       attr_reader :request, :response, :controller
 
@@ -29,11 +143,6 @@ module RSpec
         request.env['RAW_POST_DATA']=body
       end
 
-      # @private
-      def controller_class
-        described_class
-      end
-
       included do
         subject { @controller }
 
@@ -43,7 +152,7 @@ module RSpec
           @request = ActionController::TestRequest.new
           @response = ActionController::TestResponse.new
 
-          if klass = controller_class
+          if klass = self.class.controller_class
             @controller ||= klass.new rescue nil
           end
 
